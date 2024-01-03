f6e77e7a5039ef3aa3666d876befc6dae02da1f0
leeyoujun <muenzz119@naver.com>
2024-01-03 22:59:12 +0900

    chore: 포트 오타 수정

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(PORT);
 }
 bootstrap();
