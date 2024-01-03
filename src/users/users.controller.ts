2c14e52fe9a04d697c286d7b1b7e7f55e6a02d2c
yjlee <yjlee@seowoninfo.com>
2024-01-03 18:11:12 +0900

    add: User Module and Controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('users')
+// Compare this snippet from src/users/users.controller.ts:
+export class UsersController {
+  constructor() {}
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { get } from 'http';
+
+@Injectable()
+export class UsersService {
+  // 유저 전체 조회
+  findAll() {
+    return 'This action returns all users';
+  }
+  // 유저 개별 조회
+  findOne() {
+    return 'This action returns a #${id} user';
+  }
+  // 유저 생성
+  create() {
+    return 'This action adds a new user';
+  }
+  // 유저 업데이트
+  update() {
+    return 'This action updates a #${id} user';
+  }
+  // 유저 삭제
+  delete() {
+    return 'This action removes a #${id} user';
+  }
+}
