0ebe516e3e1f0e658800a78f337411af156330b3
yjlee <yjlee@seowoninfo.com>
2024-01-04 17:37:37 +0900

    fix: 몽구스 테스트 불통과함

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { Model } from 'mongoose';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -9,7 +10,13 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: 'UserModel', // Replace 'UserModel' with the actual name of your UserModel
+          useValue: Model, // Replace 'Model' with the actual implementation of your UserModel
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
